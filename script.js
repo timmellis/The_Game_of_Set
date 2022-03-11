2b6c8966e6e991ff14a238c3341c972d482e8ce6
timmellis <tim.m.ellis@gmail.com>
2022-03-11 12:05:55 -0500

    22: code cleanup

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // **** USE THIS TO SET THE LENGTH OF THE "DEAL CARD" ANIMATION
 const FLIP_ANIM_DURATION = 1; 
 
+//////////////////////////////////////////////////////
+//////////////////// DOM ELEMENTS //////////////////// 
+//////////////////////////////////////////////////////
+
 const gameBoard = document.querySelector("#game-board");
 const sidebar = document.querySelector("#game-sidebar");
 const setCounter = document.querySelector("#set-count");
@@ -16,12 +20,9 @@ const dom_hintBtn = document.querySelector("#hint-btn");
 const dom_hintText = document.querySelector("#hint-text");
 
 
-const options = {
-  "number": [1, 2, 3],
-  "color": ["A","B","C"],
-  "shape": ["diamond","oval","numeral"],
-  "fill": ["filled","empty","striped"]
-};
+//////////////////////////////////////////////////////
+////////////////// GLOBAL VARIABLES ////////////////// 
+//////////////////////////////////////////////////////
 
 class Card {
   constructor(number, color, fill, shape, id, id_num) {
@@ -35,6 +36,14 @@ class Card {
   }
 }
 
+// DO NOT EDIT: Css rules are based on these naming conventions
+const options = {
+  "number": [1, 2, 3],
+  "color": ["A","B","C"],
+  "shape": ["diamond","oval","numeral"],
+  "fill": ["filled","empty","striped"]
+};
+
 let deck = [];
 let onTheBoard = [];
 let selectedCards = [];
@@ -47,7 +56,12 @@ let solutions = [];
 let currentHint = [];
 
 
-// Generate the complete deck
+
+//////////////////////////////////////////////////////
+/////////////////// SETUP FUNCTIONS ////////////////// 
+//////////////////////////////////////////////////////
+
+// Function to GENERATE the COMPLETE DECK
 function makeDeck() {
   let c = 1;
   const keys = Object.keys(options);
@@ -63,8 +77,15 @@ function makeDeck() {
     }
   }
 }
-makeDeck();
+makeDeck();         // CALL the MAKEDECK FUNCTION to build deck
 
+
+
+//////////////////////////////////////////////////////
+////////////////////// FUNCTIONS ///////////////////// 
+//////////////////////////////////////////////////////
+
+// CORE FUNCTION for DRAWING CARDS:
 function draw() {
   let n = Math.floor(Math.random() * deck.length);
   let drawnCard = deck[n];
@@ -73,7 +94,7 @@ function draw() {
   return drawnCard;
 }
 
-// DRAW SET OF THREE CARDS:
+// CORE FUNCTION to DRAW 3X CARDS:
 function drawThree() {
   let drawSet = [];
   for (let i=0; i < 3; i++) {
@@ -82,10 +103,14 @@ function drawThree() {
   return drawSet;
 }
 
+// FUNCTION to determine board size, draw new cards if less than 12
 function checkBoardSize() {
   return onTheBoard.length < 12 ? dom_drawThreeButton.click() : true;
 }
 
+///////////////////////////////////
+///// WIN CONDITION FUNCTIONS /////
+///////////////////////////////////
 
 function winCheckNum(arr) {
   if (arr[0].number == arr[1].number && arr[1].number == arr[2].number) return true;
@@ -108,32 +133,42 @@ function winCheckFill(arr) {
     else return false;
 }
 
+// FUNCTION to check SELECTED CARDS against ALL WIN CONDITIONS
 function checkForSet(arr) {
-    // Check for Game Mode:
-    if (gameMode == "expert") return winCheckNum(arr) && winCheckColor(arr) &&  winCheckShape(arr) && winCheckFill(arr);
-    else if (gameMode == "intermediate") return winCheckNum(arr) && winCheckColor(arr) &&  winCheckShape(arr);
-    else if (gameMode == "novice") return winCheckNum(arr) && winCheckColor(arr);
-    else if (gameMode == "basic") return winCheckNum(arr);
+  // Check win conditions, as required by Game Mode:
+  if (gameMode == "expert") return winCheckNum(arr) && winCheckColor(arr) &&  winCheckShape(arr) && winCheckFill(arr);
+  else if (gameMode == "intermediate") return winCheckNum(arr) && winCheckColor(arr) &&  winCheckShape(arr);
+  else if (gameMode == "novice") return winCheckNum(arr) && winCheckColor(arr);
+  else if (gameMode == "basic") return winCheckNum(arr);
 }
 
+// FUNCTION TO FORMALLY SUBMIT A SET TO BE CHECKED and COUNTED
 function submitASet(arr) {
   const selectedDomCards = document.querySelectorAll(".selected");
   const isASet = checkForSet(arr);
-
+  
+  
   if (checkForSet(arr)) {
+  
+    ///////////////////////
+    // IF THE SET IS VALID:
+  
     foundSets.push(selectedCards);
   
+    // Update "Sets Found" counter & DOM
     setCounter.innerText = foundSets.length;
 
+    // Update "Score" counter & DOM
     scoreCounter += (3*gameModeMult);
     scoreCounter < 0 ? scoreCounter = 0 : scoreCounter;
     dom_scoreCount.innerText = scoreCounter;
 
-      const setFoundWrapper = document.createElement("div");
-      setFoundWrapper.setAttribute("class","set-found-wrapper");
 
-        // FLIP STATE SAVE:
-        const state = Flip.getState(".card"); // *** <-- ANIMATION PRE-STATE
+    // ADD CARD DIVS TO "Sets Found" container  
+    const setFoundWrapper = document.createElement("div");                                      
+    setFoundWrapper.setAttribute("class","set-found-wrapper");
+
+    const state = Flip.getState(".card"); // *** <-- FLIP ANIMATION PRE-STATE
 
     selectedDomCards.forEach(e => {
       //const thisWidth = e.offsetWidth;
@@ -150,12 +185,12 @@ function submitASet(arr) {
         c.style.backgroundSize = "4px 4px";
         c.classList.contains("shape-numeral") ? c.style.borderRadius = "1px" : null;
       });
-
     });
+
     setsFoundList.prepend(setFoundWrapper);
 
+    // Remove those cards from js array "onTheBoard"
     let ontheboardCardIndexes = [];
-
     onTheBoard.forEach((e,i) => {
       if (arr.includes(e)) onTheBoard[i] = null;
     }) // End forEach loop
@@ -178,13 +213,15 @@ function submitASet(arr) {
     dom_hintBtn.innerText = "Need a hint?";
     dom_hintText.style.animation = "fade-out 2s forwards";
   
-  } else { 
+  }         // END if SET IS VALID 
+
+  else {    // ELSE (SET is invalid)
     incorrectCounter++;
     selectedDomCards.forEach(e => {
       e.classList.add("selected-wrong");
     });
     document.querySelector("#incorrect-counter").innerText = incorrectCounter;
-  }
+  }         // END else (SET invalid)
 
   // After win OR lose, for each selected card: remove class "selected" from DOM,
   // and reset object's "isActive" property to "false"; then empty selectedCards array.
@@ -200,9 +237,13 @@ const unsetSelecedCards = () => {
 if (isASet) unsetSelecedCards();
 else setTimeout(() => {unsetSelecedCards()}, 500);
 
-} // end "checkForSet()" function
+}         // end "checkForSet()" function
 
 
+
+///////////////////////////////
+///// COMPUTER DETERMINED ///// 
+///// SETS ON THE BOARD ///////
 
 function checkForSolutions() {
   solutions = [];
@@ -218,16 +259,24 @@ function checkForSolutions() {
     }
   })
   return solutions;
-
-}
-
+}         // End "checkForSolutions()" function
 
 
+///// RESET FUNCTION /////
 
-/* ********** YOU CAN ONLY HAVE ONE OF THESE ACTIVE AT ONCE!!! ********** */
-/* ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓  */
+function fullReset() {
+  if (prompt("Are you sure you want to reset the game?")) {
+    document.querySelectorAll(".card").remove();
+    gameBoard.innerHTML = "";
+  }
+}         // End fullReset() function
 
-/* THIS IS THE DEALCARDS FUNCTION FOR GRID LAYOUT */
+
+
+//////////////////////////////////////
+/* DEALCARDS FUNCTION (GRID LAYOUT) */
+//////////////////////////////////////
+
 function dealCards(arr) {
   arr.forEach(e => {
     // Create a card-wrapper div, set class and id
@@ -246,29 +295,29 @@ function dealCards(arr) {
     thisShape.setAttribute("id",`${e.id}-${i}`); // Set unique id.
     thisShape.setAttribute("data-shape-mult", `${i}`);
     
-      // DUMMY TEXT JUST TO SEE THAT IT'S WORKING (before styling for shapes is complete)
-      // thisShape.innerText = `Demo: ${e.id}`;     
-
       domCard.appendChild(thisShape);       // Append the current shape div to card-wrapper div
-
     } 
     gameBoard.appendChild(domCard);        // Append ALL of that to DOM gameBoard.
 
-                                            // *** DOM ADD ***
-    domCard.classList.add("card-new");      // <-- Added for the sake of the animation
-    // FLIP STATE SAVE:
-    const dealState = Flip.getState(".card"); // *** <-- ANIMATION PRE-STATE
-    domCard.classList.remove("card-new");      // <-- Added for the sake of the animation
-    // animate from the previous state to the current one:  
-    Flip.from(dealState, {duration: FLIP_ANIM_DURATION, ease: "power1.inOut", absolute: true
-    //onComplete: myFunc
-    });
+    ///// ADD TO DOM /////
 
-   
-   
+    domCard.classList.add("card-new");      // <-- Added for the sake of the animation
+    
+    const dealState = Flip.getState(".card"); // *** <-- FLIP ANIMATION PRE-STATE
+    domCard.classList.remove("card-new");      // <-- Added for the sake of the animation
+
+    // animate from the previous state to the current one:  
+    Flip.from(dealState, {duration: FLIP_ANIM_DURATION, ease: "power1.inOut", absolute: true});
+
     onTheBoard.push(e);                     // Adds card object to the onTheTable array. 
 
+    ///////////////////////////////////////////
+    ///// ADD EVENT LISTENER TO EACH CARD /////
+    ///// AS IT IS ADDED TO THE DOM ///////////
+
     domCard.addEventListener('click', () => {
+      
+      // Update "isActive" value in the Card object itself
       e.isActive = !e.isActive;
 
       // Check if e is in the SelectedCards array: if not, add it; if yes, find it and remove it.
@@ -289,76 +338,15 @@ function dealCards(arr) {
   })
 }
 
-/* THIS IS THE DEALCARDS FUNCTION FOR FLEX MATRIX LAYOUT */
-// function dealCards(arr) {       
-//   const domCardCol = document.createElement("div");       // Create a card column for each set of 3 cards
-//   domCardCol.setAttribute("class","card-col-flex-wrapper");
-
-//   arr.forEach(e => {
-
-//     const domCard = document.createElement("div");        // Create a card-wrapper div, set class and id
-//     domCard.setAttribute("class","card card-wrapper");
-//     domCard.setAttribute("id", `${e.id}`);
-
-//     // Create and append "shape" div n number of times, based on current object's "number" value
-//     let n = parseInt(e.number);
-//     for (let i=0; i < n; i++) {
-    
-//       //Create a shape div according to the current object specs  
-//     const thisShape = document.createElement("div");
-//     thisShape.setAttribute("class",`shape-object color-${e.color} fill-${e.fill} shape-${e.shape}`);
-//     thisShape.setAttribute("id",`${e.id}-${i}`); // Set unique id.
-    
-//       // DUMMY TEXT JUST TO SEE THAT IT'S WORKING (before styling for shapes is complete)
-//       // thisShape.innerText = `Demo: ${e.id}`;     
-
-//       domCard.appendChild(thisShape);       // Append the current shape n times to card div (to make 1, 2, or 3 shapes on a card)
-//     } 
-
-//     domCardCol.appendChild(domCard);      // Append each card to the domCardColumn
-//     onTheBoard.push(e);                     // Adds card object to the onTheBoard array. 
-
-//     // ADDS THE EVENT LISTENER TO EACH INDIVIDUAL CARD ONCE IT IS CREATED
-//     domCard.addEventListener('click', () => {
-//       console.log(`you clicked ${e.id}`);
-//       e.isActive = !e.isActive;
-//                 console.log("current card object:", e, "current card.isActive:", e.isActive);
-
-//       // Check if e is in the SelectedCards array: if not, add it; if yes, find it and remove it.
-//       if (!selectedCards.includes(e)) selectedCards.push(e);
-//        else if (selectedCards[0] == e) selectedCards.shift();
-//         else selectedCards.pop();
-//                   console.log("Selected cards array:", selectedCards);
-
-//       e.isActive ? domCard.classList.add("selected") : domCard.classList.remove("selected");
-
-//       // If this was the 3rd card, check for win conditions
-//       if (selectedCards.length > 2) {
-//         checkForSet(selectedCards);
-//       }
-//     })
-//   })
-//   gameBoard.appendChild(domCardCol);      // Finally, append ALL of that to DOM gameBoard.
-// }
-
-/* ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑  */
-/* ********** YOU CAN ONLY HAVE ONE OF THESE ACTIVE AT ONCE!!! ********** */
-
-
-function fullReset() {
-  if (prompt("Are you sure you want to reset the game?")) {
-    document.querySelectorAll(".card").remove();
-    gameBoard.innerHTML = "";
-  }
-}
 
 
 
 
+/////////////////////////////////////////////////////////
+//////////////////// EVENT LISTENERS //////////////////// 
+/////////////////////////////////////////////////////////
 
-/////////////////////// EVENT LISTENERS
-
-// TO START THE GAME AND DRAW 12 CARDS
+// DEAL BUTTON: START THE GAME AND DRAW 12 CARDS
 dom_dealButton.addEventListener("click", () => {
   for (let i=0; i < 4; i++) {
     dealCards(drawThree());
@@ -390,16 +378,19 @@ dom_dealButton.addEventListener("click", () => {
   checkForSolutions();
 });
 
-// FOR ALL SUBSEQUENT CARD DRAWS, INCLUDING COMPUTER-INITIATED ONES:
+// DRAW-THREE BUTTON: FOR ALL SUBSEQUENT CARD DRAWS, INCLUDING COMPUTER-INITIATED ONES:
 dom_drawThreeButton.addEventListener("click", () => {
   dealCards(drawThree());
 });
 
-// FOR THE HINT BUTTON
+
+// HINT BUTTON: GENERATE SOLUTIONS AND PICK ONE TO SHARE WITH PLAYER //
 dom_hintBtn.addEventListener("click", () => {
   
-  // ON first HINT click:
-  if (!currentHint.length) {
+  if (!currentHint.length) {        
+    
+    //////////////////////
+    // ON FIRST HINT click 
 
     scoreCounter -= gameModeMult;   // penalty for taking a 1st hint (1/3 of points)
     
@@ -435,13 +426,13 @@ dom_hintBtn.addEventListener("click", () => {
       else if (numSame) hint1 += "You can make a Set using all the same number..."
       else hint1 += "You can make a Set where ALL elements are different...";
     }
-    console.log(hint1);
     dom_hintText.innerText = hint1;
     dom_hintText.style.animation = "fade-in 4s";
     dom_hintBtn.innerText = "Need a bigger hint?";
 
-  } // End first hint section
-  else {        // second hint section 
+  }             // End first hint section
+  
+  else {        // Start second hint section 
 
     scoreCounter -= 2*gameModeMult;   // penalty for taking a 2nd hint (2/3 of points)
 
@@ -449,16 +440,5 @@ dom_hintBtn.addEventListener("click", () => {
     const cardHint2 = document.querySelector(`[data-id-num="${currentHint[1].id_num}"]`);
     cardHint1.style.animation = "glow-fade-in-out 3s infinite";
     cardHint2.style.animation = "glow-fade-in-out 3s infinite";
-  }
+  }             // End second hint section
 });
-
-//dom_dealButton.click();
-
-
-
-
-
-
-
-
-
